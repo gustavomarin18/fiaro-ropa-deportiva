214150d03ec84552f73b0e352f06b35a439a103d
gustavomarin18 <marinproducciones81@gmail.com>
2022-02-12 20:41:43 -0300

    le di estilo a item y itemList-Marin

--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
+import React from "react";
+import "../styles/Item.css";
+import { Button } from "react-bootstrap";
 
-
-const Item = ({item}) => {
+const Item = ({ item }) => {
   return (
-    <div>
-
-<h1>{item.title}</h1>
-<p>{item.description}</p>
-<h4>{item.price}</h4>
-<img src={item.pictureUrl} alt="IMG-0061" border="0"></img>
-<br></br>
-<button>Ver detalle del producto</button>
-<p>Stock disponible{item.stock}</p>
-
-
-
-
-
-
-
-
+    <div className="items">
+      <h1 className="item-title">{item.title}</h1>
+      <p className="item-description">Descripción:{item.description}</p>
+      <h4 className="item-price">Precio:{item.price}$</h4>
+      <img
+        className="img-item"
+        src={item.pictureUrl}
+        alt="IMG-0061"
+        border="0"
+      ></img>
+      <br></br>
+      <Button className="item-button" variant="info">
+        Ver detalle del producto
+      </Button>
+      <p className="item-stock">Stock disponible: {item.stock}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Item
+export default Item;
--- src/components/ItemListContainer/ItemList.jsx
+++ src/components/ItemListContainer/ItemList.jsx
@@ -1,60 +1,55 @@
 import React from "react";
 import Item from "./Item";
 import { useEffect, useState } from "react";
+import "../styles/ItemList.css";
 
 const ItemList = () => {
   const products = [
     {
       id: 1,
-      title: "biker",
+      title: "Biker",
       name: "kenia",
-     description: "Calza biker",
+      description: "Calza biker",
       price: 2580,
       stock: 30,
-      pictureUrl:
-        "https://i.ibb.co/5sB9HY8/calza-biker-kenia.jpg",
+      pictureUrl: "https://i.ibb.co/5sB9HY8/calza-biker-kenia.jpg",
     },
     {
       id: 2,
-      title: "Buzos",
+      title: "Buzo",
       name: "Africa",
-     description: "Buzo-Rosa viejo",
+      description: "Buzo-Rosa viejo",
       price: 2730,
       stock: 20,
-      pictureUrl:
-      "https://i.ibb.co/f2Sbnm0/Buzo-Africa.jpg",
+      pictureUrl: "https://i.ibb.co/f2Sbnm0/Buzo-Africa.jpg",
     },
     {
       id: 3,
-      title: "Calzas",
+      title: "Calza",
       name: "Mali",
-     description: "Calza 3/4 lycra",
+      description: "Calza 3/4 lycra",
       price: 2100,
       stock: 10,
-      pictureUrl:
-        "https://i.ibb.co/YR5rgws/calza-capri-mali.jpg",
+      pictureUrl: "https://i.ibb.co/YR5rgws/calza-capri-mali.jpg",
     },
     {
       id: 4,
       title: "Remera",
       name: "Sabah",
-     description: "Remera con capucha termica",
+      description: "Remera con capucha termica",
       price: 2980,
       stock: 40,
-      pictureUrl:
-        "https://i.ibb.co/fMRrzJ5/remera-sabah.jpg",
+      pictureUrl: "https://i.ibb.co/fMRrzJ5/remera-sabah.jpg",
     },
     {
       id: 5,
-      title: "Tops",
+      title: "Top",
       name: "Sicilia",
-     description: "Top con mangas",
+      description: "Top con mangas",
       price: 1980,
       stock: 6,
-      pictureUrl:
-        "https://i.ibb.co/CQxqghS/top-sicilia.jpg",
+      pictureUrl: "https://i.ibb.co/CQxqghS/top-sicilia.jpg",
     },
-
   ];
 
   const [productos, setProductos] = useState([]);
@@ -78,12 +73,10 @@ const ItemList = () => {
     getData(products)
       .then((res) => setProductos(res))
       .catch((err) => console.log(err));
-  }, );
+  });
 
   return (
-    <div>
-      {/* {productos.map((item) =>(<Item key= {item.id} item={item}/>))} */}
-
+    <div className="itemList-design">
       {productos.length ? (
         productos.map((producto) => <Item item={producto} key={producto.id} />)
       ) : (
